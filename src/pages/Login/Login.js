06c7b93e7c8c97a7b3ef7252a92efce1ec57b7f5
yigitkocturk <yigitkocturkk@gmail.com>
2023-04-14 14:09:53 +0300

    add a auth control and some bugs fixed

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SafeAreaView, View, Image, Alert} from 'react-native';
 import {Formik} from 'formik';
+import { useDispatch } from 'react-redux';
 import styles from './login.style';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
@@ -8,25 +9,23 @@ import Config from 'react-native-config';
 import usePost from '../../hooks/usePost/usePost';
 const Login = ({navigation}) => {
   const {data, post, loading, error} = usePost();
+  const dispatch = useDispatch();
 
   function handleLogin(values) {
-    post(Config.API_AUTH_URL + '/login',values);
+    post(Config.API_AUTH_URL + '/login', values);
   }
 
-  if(error){
-    Alert.alert('Dukkan', 'Bir hata oluştu')
+  if (error) {
+    Alert.alert('Dukkan', 'Bir hata oluştu');
   }
 
-  if(data){
-    if(data.status === 'Error') {
-        Alert.alert('Dukkan', 'Kullanıcı bulunamadı!')
-    }else {
-        navigation.navigate('ProductsPage');
+  if (data) {
+    if (data.status === 'Error') {
+      Alert.alert('Dukkan', 'Kullanıcı bulunamadı!');
+    } else {
+      dispatch({type: 'SET_USER' , payload: {user}})
     }
-   
-  } 
-
-
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -63,3 +62,20 @@ const Login = ({navigation}) => {
 };
 
 export default Login;
+
+const user = {
+  address: {
+    geolocation: {lat: '-37.3159', long: '81.1496'},
+    city: 'kilcoole',
+    street: 'new road',
+    number: 7682,
+    zipcode: '12926-3874',
+  },
+  id: 1,
+  email: 'john@gmail.com',
+  username: 'johnd',
+  password: 'm38rmF$',
+  name: {firstname: 'john', lastname: 'doe'},
+  phone: '1-570-236-7033',
+  __v: 0,
+};
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,10 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {FlatList, ActivityIndicator} from 'react-native';
 import Config from "react-native-config";
 import ProductCard from '../../components/ProductCard/ProductCard';
 import useFetch from '../../hooks/useFetch/useFetch';
 
+
+
 const Products = ({navigation}) => {
+  
   const {loading, data, error} = useFetch(Config.REACT_APP_API_URL);
 
   const handleProductSelect = id => {
@@ -23,7 +26,11 @@ const Products = ({navigation}) => {
     return <Text>{error} </Text>;
   }
 
-  return <FlatList data={data} renderItem={renderProduct} />;
+  return <FlatList data={data} renderItem={renderProduct} />
+   
+  
+  
+ 
 };
 
 export default Products;
